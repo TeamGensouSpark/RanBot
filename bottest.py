95ac9b7d23b6efa20f810124d95c32111388b7a9
IAXRetailer <h2sxxa0w0@gmail.com>
2022-12-25 19:37:58 +0800

    fix the foolish IDE modify

--- bottest.py
+++ bottest.py
@@ -1,11 +1,11 @@
 from typing import TYPE_CHECKING, Set
-import bottest
+import pytest
 
 if TYPE_CHECKING:
     from nonebot.plugin import Plugin
 
 
-@bottest.fixture
+@pytest.fixture
 def load_plugins(nonebug_init: None) -> Set["Plugin"]:
     import nonebot
     return nonebot.load_plugins("src/plugins")
